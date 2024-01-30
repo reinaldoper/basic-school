db5b2bb80e158161f1e633d8494fe87eff66aec3
reinaldoper <reinaldoper83@gmail.com>
2024-01-30 13:15:59 -0400

    refactored: ajusta css

--- src/component/ListAlunos.tsx
+++ src/component/ListAlunos.tsx
@@ -22,9 +22,9 @@ const ListAlunos = () => {
 
   const listAlunos = alunos.map(aluno => (
     <ol key={aluno.id} id="w3-ul" className="w3-container w3-animate-top">
-      <li>aluno: {aluno.nome}</li>
+      <li>student: {aluno.nome}</li>
       <li id="list-between">teacher: {aluno.professor.nome}</li>
-      <li id='list-email'>disciplina: {aluno.professor.disciplina}</li>
+      <li id='list-email'>discipline: {aluno.professor.disciplina}</li>
     </ol>
   ));
 
