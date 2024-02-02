ee5264d85d2e7b6368f21959b0990f9b121cb3a6
reinaldoper <reinaldoper83@gmail.com>
2024-02-01 20:11:05 -0400

    feat: adiciona paginations

--- src/component/ListAlunos.tsx
+++ src/component/ListAlunos.tsx
@@ -1,11 +1,32 @@
 import { useState } from "react";
 import { useStore } from "../store/state";
+import Pagination from 'react-js-pagination';
 
 const ListAlunos = () => {
   const [modalIsOpen, setIsOpen] = useState<boolean>(false);
   const [selectedStudentId, setSelectedStudentId] = useState<number | null>(null);
 
   const listAluno = useStore((state) => state.aluno)
+
+  const [activePage, setActivePage] = useState(1);
+  const itemsPerPage = 4;
+  const totalItemsCount = listAluno.length;
+  const pageRangeDisplayed = 2;
+
+
+
+  const handlePageChange = (pageNumber: number) => {
+    setActivePage(pageNumber);
+  };
+
+  const sortedData = listAluno.sort((a, b) =>
+    a.nome.toLowerCase().localeCompare(b.nome.toLowerCase())
+  );
+
+  const paginatedData = sortedData.slice(
+    (activePage - 1) * itemsPerPage,
+    activePage * itemsPerPage
+  );
 
 
   const handleStudentClick = (id: number) => {
@@ -21,7 +42,7 @@ const ListAlunos = () => {
 
   const renderStudentDetails = () => {
     if (selectedStudentId !== null) {
-      const result = listAluno.find((student) => student.id === selectedStudentId);
+      const result = paginatedData.find((student) => student.id === selectedStudentId);
 
       return result?.notas?.map((aluno) => (
         <ol id="w3-ul" className="w3-container w3-animate-top">
@@ -41,7 +62,7 @@ const ListAlunos = () => {
   };
 
 
-  const listAlunos = listAluno.length && listAluno.map(aluno => (
+  const listAlunos = paginatedData.length && paginatedData.map(aluno => (
     <ol key={aluno.id} id="w3-ul" className="w3-container w3-animate-top">
       <li>student: {aluno.nome}</li>
       <li id="list-between">teacher: {aluno.professor.nome}</li>
@@ -56,8 +77,22 @@ const ListAlunos = () => {
   return (
     <>
       <div className="render-teacher">
-        <h2 className="w3-cursive">{listAluno && !modalIsOpen ? 'Lista de alunos:' : 'Aluno(a):'}</h2>
+        <h2 className="w3-cursive">{listAluno && !modalIsOpen ? 'Lista de alunos:' : renderStudentDetails()?.length ? 'Aluno(a):' :
+          <button type="button" className="fa fa-close" onClick={closeModal}></button>
+        }</h2>
         {listAlunos && !modalIsOpen ? <>{listAlunos}</> : <>{renderStudentDetails()}</>}
+        {totalItemsCount > itemsPerPage ? 
+        <div className="pagination">
+          <Pagination
+            itemClass='pagination'
+            activePage={activePage}
+            itemsCountPerPage={itemsPerPage}
+            totalItemsCount={totalItemsCount}
+            pageRangeDisplayed={pageRangeDisplayed}
+            onChange={handlePageChange}
+          />
+
+        </div> : null}
       </div>
     </>
   )
--- src/component/ListProfessores.tsx
+++ src/component/ListProfessores.tsx
@@ -1,12 +1,34 @@
 import { useStore } from "../store/state";
 import { useState } from "react";
 import '../styles/home.css'
+import Pagination from 'react-js-pagination';
+
 
 const ListProfessores = () => {
   const [modalIsOpen, setIsOpen] = useState<boolean>(false);
   const [selectedTeacherId, setSelectedTeacherId] = useState<number | null>(null);
 
   const teacherResult = useStore((state) => state.disciplina);
+
+  const [activePage, setActivePage] = useState(1);
+  const itemsPerPage = 4;
+  const totalItemsCount = teacherResult.length;
+  const pageRangeDisplayed = 2;
+
+
+
+  const handlePageChange = (pageNumber: number) => {
+    setActivePage(pageNumber);
+  };
+
+  const sortedData = teacherResult.sort((a, b) =>
+    a.nome.toLowerCase().localeCompare(b.nome.toLowerCase())
+  );
+
+  const paginatedData = sortedData.slice(
+    (activePage - 1) * itemsPerPage,
+    activePage * itemsPerPage
+  );
 
   const handleTeacherClick = (id: number) => {
     setIsOpen(true);
@@ -20,7 +42,7 @@ const ListProfessores = () => {
 
   const renderTeacherDetails = () => {
     if (selectedTeacherId !== null) {
-      const result = teacherResult.find((teacher) => teacher.id === selectedTeacherId);
+      const result = paginatedData.find((teacher) => teacher.id === selectedTeacherId);
 
       return result?.alunos?.map((student) => (
         <ol id="w3-ul" className="w3-container w3-animate-top">
@@ -37,7 +59,7 @@ const ListProfessores = () => {
     return null;
   };
 
-  const listTeacher = teacherResult.length && teacherResult.map((teacher) => (
+  const listTeacher = paginatedData.length && paginatedData.map((teacher) => (
     <ol key={teacher.id} id="w3-ul" className="w3-container w3-animate-top">
       <li id="teacher-name">{teacher.nome}</li>
       <li id="list-between">{teacher.disciplina}</li>
@@ -53,6 +75,18 @@ const ListProfessores = () => {
       <div className="render-teacher">
         <h2 className="w3-cursive">{listTeacher && !modalIsOpen ? 'Lista de professores:' : 'Aluno(a)s:'}</h2>
         {listTeacher && !modalIsOpen ? <>{listTeacher}</> : <>{renderTeacherDetails()}</>}
+        {totalItemsCount > itemsPerPage ? 
+        <div className="pagination">
+          <Pagination
+            itemClass='pagination'
+            activePage={activePage}
+            itemsCountPerPage={itemsPerPage}
+            totalItemsCount={totalItemsCount}
+            pageRangeDisplayed={pageRangeDisplayed}
+            onChange={handlePageChange}
+          />
+
+        </div> : null}
       </div>
     </>
   );
