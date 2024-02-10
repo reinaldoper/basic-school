700eebf5fe0812dcff3efb1a0d39316b648b97be
reinaldoper <reinaldoper83@gmail.com>
2024-02-09 20:22:19 -0400

    refactored: adiciona condição de deletar professor"

--- src/component/FormDelTeacher.tsx
+++ src/component/FormDelTeacher.tsx
@@ -1,6 +1,13 @@
-
+import { useStore } from "../store/state"
+import { useNavigate } from "react-router-dom"
 
 const FormDelTeacher = () => {
+  const navigate = useNavigate();
+
+  const dir = useStore(state => state.dir)
+
+  if(!dir) navigate('/');
+
   return (
     <>
       <h1>Teacher</h1>
