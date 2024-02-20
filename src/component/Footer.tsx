8f23889f6fb219d2af42c838aac25fe9b89db32a
reinaldoper <reinaldoper83@gmail.com>
2024-02-20 07:48:59 -0400

    refactored: refatora compoennt footer

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,27 +1,13 @@
 import livros from '../assets/livros.jpg';
 import homem from '../assets/homem.jpg';
+import { SectionFooter, SectionFooterTwo } from '../sections/Sections';
 
 
 const Footer = () => {
   return (
     <>
-      <section id="professional-challenges">
-        <h2>Venha fazer parte do nosso mundo.</h2>
-        <img src={ homem } 
-        title="Homem lendo em um tablet" alt="Homem lendo" />
-        <p>
-          Junte-se a nós. Aqui você encontra os melhores professores, os melhores livros,
-          salas de aula dotadas com a mais alta tecnologia do momento.
-          Livraria digital bem atualizada e com os melhores acervos do mundo.
-        </p>
-      </section>
-      <footer className="w3-container w3-teal footer-div">
-        <img className="logo" src={livros}
-          title="Logotipo"
-          alt="Logotipo" />
-        <p>Acesse <a className="footer-a" href="https://github.com/reinaldoper">o meu</a> portfolio.</p>
-        <p>Todos os direitos reservados a &copy;Copyright</p>
-      </footer>
+      {SectionFooter(homem)}
+      {SectionFooterTwo(livros)}
     </>
 
   )
--- src/sections/Sections.tsx
+++ src/sections/Sections.tsx
@@ -50,3 +50,31 @@ export const SectionHome = () => {
     </section>
   )
 }
+
+
+export const SectionFooter = (homem: string) => {
+  return (
+    <section id="professional-challenges">
+      <h2>Venha fazer parte do nosso mundo.</h2>
+      <img src={homem}
+        title="Homem lendo em um tablet" alt="Homem lendo" />
+      <p>
+        Junte-se a nós. Aqui você encontra os melhores professores, os melhores livros,
+        salas de aula dotadas com a mais alta tecnologia do momento.
+        Livraria digital bem atualizada e com os melhores acervos do mundo.
+      </p>
+    </section>
+  )
+}
+
+export const SectionFooterTwo = (livros: string) => {
+  return (
+    <footer className="w3-container w3-teal footer-div">
+      <img className="logo" src={livros}
+        title="Logotipo"
+        alt="Logotipo" />
+      <p>Acesse <a className="footer-a" href="https://github.com/reinaldoper">o meu</a> portfolio.</p>
+      <p>Todos os direitos reservados a &copy;Copyright</p>
+    </footer>
+  )
+}
