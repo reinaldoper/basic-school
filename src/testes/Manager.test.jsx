ae92d3105b78adb720ef4794bc9eea0d27e7c638
reinaldoper <reinaldoper83@gmail.com>
2024-02-22 12:57:24 -0400

    feat: adiciona test component Salles

---
+++ src/testes/Manager.test.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Salles from '../component/Salles'
+import { manager } from '../../.jest/mock/AboutText';
+
+
+
+const nome = 'Moreira Salles'
+const email = 'moreira@school.com'
+const role = 'DIR'
+
+const diretor = [
+  nome, email, role
+]
+
+
+
+jest.mock('../store/state', () => ({
+  diretor,
+  setAddUser: jest.fn(),
+  useStore: jest.fn(() => ({
+    diretor,
+  })),
+}));
+
+
+
+
+
+
+describe('Footer', () => {
+  it('should return footer component.', async () => {
+    render(
+      <MemoryRouter>
+        <Salles />
+      </MemoryRouter>
+    );
+    const renderText = screen.getByText('Carregando...')
+    expect(renderText).toBeInTheDocument();
+
+
+    await waitFor(() => {
+      const renderMsg = screen.getByText(manager);
+      expect(renderMsg).toBeInTheDocument();
+    })
+  });
+});
