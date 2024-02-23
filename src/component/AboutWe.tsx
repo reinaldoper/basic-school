dcc514bede422186d519c6087c10f4da32e5bd33
reinaldoper <reinaldoper83@gmail.com>
2024-02-23 13:51:17 -0400

    refactored: ajusta test aboutWe

--- src/component/AboutWe.tsx
+++ src/component/AboutWe.tsx
@@ -1,20 +1,19 @@
 import '../styles/home.css'
-import { useStore } from "../store/state";
 import { AboutSections } from '../sections/Sections';
+import Stats from '../utils/Stats';
 
 const AboutWe = () => {
 
-  const diretor = useStore((state) => state.user)
+  const start = Stats()
 
-  console.log(diretor);
-  
+  const { userLogedIn } = start;
 
   return (
     <>
       <p className='w3-container w3-animate-top about-we'>
         <AboutSections />
         <span>
-          {diretor.length ? diretor[0].nome : <p>Carregando...</p>}<br />
+          {userLogedIn.length ? userLogedIn[0].nome : <p>Carregando...</p>}<br />
           Diretor da Escola Basic School
           <p>Todos os direitos reservados a &copy;Copyright</p>
         </span>
--- src/testes/AboutWe.test.jsx
+++ src/testes/AboutWe.test.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import AboutWe from '../component/AboutWe.tsx';
 import { text } from '../../.jest/mock/AboutText.jsx';
 
 
+const nome = 'Moreira Salles';
+const email = 'moreira@school.com';
+const role = 'DIR';
 
+jest.mock('../utils/Stats', () => ({
+  __esModule: true,
+  default: jest.fn(() => ({
+    userLogedIn: [{ nome: nome, email: email, role: role }]
+  })),
+}));
 
 describe('AboutWe', () => {
   it('should return text.', () => {
@@ -14,7 +23,7 @@ describe('AboutWe', () => {
         <AboutWe />
       </MemoryRouter>
     );
-    const renderText = screen.getByText('Diretor da Escola Basic School')
+    const renderText = screen.getByText(/Diretor da Escola Basic School/)
     expect(renderText).toBeInTheDocument();
 
     const textManager = screen.getByText(text);
@@ -28,9 +37,21 @@ describe('AboutWe', () => {
         <AboutWe />
       </MemoryRouter>
     );
-    const renderText = screen.getByText('Carregando...')
+    const renderText = screen.getByText(/Todos os direitos reservados a ©Copyright/)
     expect(renderText).toBeInTheDocument();
 
 
+  });
+  it('should return name manager.', async () => {
+    render(
+      <MemoryRouter>
+        <AboutWe />
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      const renderText = screen.getByText(/Moreira Salles/)
+      expect(renderText).toBeInTheDocument();
+    })
   });
 });
