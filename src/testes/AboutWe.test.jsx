66886c20ba4cb176ed0c2dd5683a2cd4bf83783e
reinaldoper <reinaldoper83@gmail.com>
2024-02-23 08:55:44 -0400

    refactored: ajusta test aboutWe

--- src/testes/AboutWe.test.jsx
+++ src/testes/AboutWe.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import AboutWe from '../component/AboutWe.tsx';
+import { text } from '../../.jest/mock/AboutText.jsx';
 
 
 describe('AboutWe', () => {
@@ -14,7 +15,8 @@ describe('AboutWe', () => {
     const renderText = screen.getByText('Diretor da Escola Basic School')
     expect(renderText).toBeInTheDocument();
 
-
+    const textManager = screen.getByText(text);
+    expect(textManager).toBeInTheDocument();
    
     
   });
